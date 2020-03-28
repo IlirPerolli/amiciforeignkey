0a5f68e776f283d876253a54dc3a3096d9a67031
Ilir Perolli <ilir_perolli@live.com>
2020-03-28 23:40:42 +0100

    main in improved design

--- main.php
+++ main.php
@@ -1,3 +1,4 @@
+
 <?php include('server.php');
 $error = "";
 // Shiko nese jane kyqur, nese po ridirektoje ne index
@@ -7,9 +8,11 @@ header('Location: index.php');
 exit();
 }
 ?>
+
 <html>
   <head>
     <title> Llogaria amici </title>
+    <?php include("bootstrap_css.php");?>
     <link rel="icon" type="image/png" href="people.png" />
     <meta name="theme-color" content="#2f476d">
     <meta property="og:image" content="img/avatar.jpg" />
@@ -17,59 +20,70 @@ exit();
     <meta name="keywords" content="amici, llogaria, amicillogaria, amici llogaria,krijo llogari, kyqu ne amici, bisedo" />
     <meta name="google-site-verification" content="BTzlSeSQ5eRBLSlE1PrhaaNGD474rk60IL-1DZ0PnFg" />
     <link rel = "stylesheet" href="index-stili.css">
-    <link rel = "stylesheet" href="plugins/aos/aos.css">
     <meta name="msapplication-navbutton-color" content="#2f476d">
     <meta name="apple-mobile-web-app-status-bar-style" content="#2f476d">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-    <style>
-    body{
-    margin:0;
-    padding:0;
-    height: 100vh;
+  <style type="text/css">
+    *{
+      padding:0;
+      margin:0;
+      font-family: SamsungSharpSans-Bold;
     }
-    </style>
-  </head>
-  <body>
-    <div id = "particles-js"><div class = "amici-teksti"> amici</div></div>
-    <script type="text/javascript" src="particles.js"></script>
-    <script type="text/javascript" src="app.js"></script>
-    <!--<img src = "512486-PIJP0A-449.jpg" class = "jingle-bell"/>-->
-    <div style = "text-align:center">
-      <div class = "max-width">
-        <div class = "titulli"><h3> Llogaria amici</h3> <br><br><br></div>
-        <div class = "foto1">
-          <img src = "img/indexphoto.png"/>
-          <!-- Foto paraprake <img src = "E9962BD5-9B1C-4232-A166-5475F93F46F3.png"/>-->
-        </div>
-        <div class = "shkrimi">
-          <h3> Llogaria amici </h3><div class = "br"><br><br><br><br><br><br><br></div>
-          <h2> Porta juaj <br>ne boten e komunikimit </h2><p> </p>
-          <h4>Eksploroni &ccedil;fare mund te beni me nje llogari amici.</h4>
-          <br><br><div class = "butonat"><div class="butoni" id = "create-account" style = "margin-right:80px">
-            <a href="create-account.php">Krijo llogari</a>
-          </div>
-          <div class="butoni">
-            <a href="login.php">Ky&ccedil;u tani</a>
-          </div></div>
-        </div>
-        <div class = "foto" data-aos="fade-left">
-          <img src = "img/indexphoto.png"/>
-          <!-- Foto paraprake <img src = "E9962BD5-9B1C-4232-A166-5475F93F46F3.png"/>-->
-          
-        </div>
-      </div>
-    </div>
-    <script src = "plugins/aos/aos.js"></script>
-    <script type="plugins/aos/aos.js"></script>
-    <script type = "text/javascript">
-    AOS.init({
-    duration:1500,
-    });
-    </script>
-    <br><br>
-    <div class = "footer">
+    body{
+      background: #FAFAFA;
+    }
+ 
+
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+  <a class="navbar-brand" href="main.php">amici llogaria</a>
+  <ul class="navbar-nav mr-auto">
+   
+    </ul>
+  <a href="create-account.php" class="btn button-navbar" >Regjistrohu</a>
+<a href="login.php" class="btn button-navbar" >Kyçu</a>
+
+</nav>
+<br>
+<div class="container-ballina">
+  <div class="ballina-pjesa1">
+    <div class="pjesa1-container">
+      <img src="img/macbook-amici.png"/>
+<h1>Platform&euml; inovative p&euml;r student&euml; </h1>
+<br>
+<a href="create-account.php" class="btn btn-light button-mobile" id="button-ballina" >Regjistrohu</a>
+<a href="login.php" class="btn btn-light button-mobile" id="button-ballina">Kyçu</a>
+
+</div>
+  </div>
+  <div class="ballina-pjesa2">
+    <img src="img/macbook-amici.png"/>
+  </div>
+</div>
+
+<div class="container-ballina-part2">
+<div class="first-column">
+   <img src="img/amici-main.png"/>
+</div>
+<div class="second-column">
+  <div class="second-column-container">
+<h1> Çfar&euml; &euml;sht&euml; amici?</h1>
+<p> Amici &euml;sht&euml; nj&euml; platform&euml; e cila u mund&euml;son p&euml;rdoruesve (kryesisht student), t&euml; ken&euml; çasje n&euml;: libra, ligj&euml;rata, t&euml; shk&euml;mbejn&euml; dokumente, t&euml; bashk&euml;bisedojn&euml; si dhe t&euml; ken&euml; qasje n&euml; m&euml;sime.</p>
+<br>
+<p> Akoma nuk keni llogari amici?</p>
+<a href="create-account.php" class="btn btn-light button-pc" id="button-ballina" >Regjistrohu tani</a>
+
+</div>
+</div>
+</div>
+<br>
+
+ <div class = "footer">
       <span class='amici' style = "font-size: 20px;"> amici</span><span class = "team" style = "font-size: 20px;"> team <span style = "color:black;font-size: 20px;">&#9400; 2020 </span></span><!--<span class='amici' id = "amici1" style = "font-size: 20px;">Gjakova</span>-->
     </div>
     <div class = "amici1"> <span class='amici' style = "font-size:15px"> amici</span><span class = "team" style = "font-size:15px; "> team <span style = "color:black;">&#9400; 2020 </span></span><!--<span class='amici' id = "amici1" style = "font-size:15px;">Gjakova</span>--></div>
-  </body>
+     <?php include("bootstrap_javascript.php");?>
+</body>
 </html>
