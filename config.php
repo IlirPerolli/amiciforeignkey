2a53a719e5df8ee2583e9eecffa56a2581b57f7b
Ilir Perolli <ilir_perolli@live.com>
2020-04-05 19:09:41 +0200

    bug fix in config file while connecting to database

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-   $db = mysqli_connect('localhost', 'root', '', 'user-registration');
+      $db = mysqli_connect('localhost', 'root', '', 'user-registration');
 if (!$db) {
-    die('Nuk mund te lidhet me serverin MySQL: ' . mysql_error());
+    die('<p> Kerkojme falje, ka ndodhur nje gabim. Beni refresh faqen per te vazhduar.</p>' . mysql_error());//Nese nuk lidhet me db
 }
 ?>
